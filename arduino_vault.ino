eb951a1398032fcde174c80b949541936a318f6b
balbazauras <staugaitis.liudas@gmail.com>
2022-10-27 14:10:49 +0300

    Formatting code

--- arduino_vault.ino
+++ arduino_vault.ino
@@ -5,33 +5,34 @@
 #include <HTTPClient.h>
 #include <arduino_secrets.h>
 
-//Wifi vars
-const char* ssid = SECRET_SSID;
-const char* password = SECRET_PASS;
+// Wifi vars
+const char *ssid = SECRET_SSID;
+const char *password = SECRET_PASS;
 
-//Sensor vars
+// Sensor vars
 int value_lower = 10;
 int value_upper = 90;
 const String sensorId = "4";
 int randomValue;
 
 // time vars
-const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = 0;
-const int   daylightOffset_sec = 0;
+const char *ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 0;
+const int daylightOffset_sec = 0;
 char buffer[sizeof "2011-10-08T07:07:09.123345Z"];
 
-//Server/websocket vars
+// Server/websocket vars
 WebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
 
-//interval vars
+// interval vars
 int intervalToCheck = 100;
 int intervalToSend = 10000;
 unsigned long previousMillisCheck = 0;
 unsigned long previousMillisSend = 0;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
   setUpWifi();
@@ -41,41 +42,49 @@ void setup() {
   webSocket.onEvent(webSocketEvent);
 }
 
-void loop() {
+void loop()
+{
   server.handleClient();
   webSocket.loop();
   String arduinoDate = setupArduinoDate();
   unsigned long now = millis();
   randomValue = random(100);
-  if ((unsigned long)(now - previousMillisCheck) > intervalToCheck) {
-    if (randomValue < value_lower || randomValue > value_upper) {
+  if ((unsigned long)(now - previousMillisCheck) > intervalToCheck)
+  {
+    if (randomValue < value_lower || randomValue > value_upper)
+    {
       Serial.println("Value exeecds limits");
       sendHttpJSON(randomValue, arduinoDate, sensorId);
     }
     sendWithWebscoket();
     previousMillisCheck = now;
   }
-  if ((unsigned long)(now - previousMillisSend) > intervalToSend) { // check if "interval" ms has passed since last time the clients were updated
+  if ((unsigned long)(now - previousMillisSend) > intervalToSend)
+  { 
     Serial.println("Normal Post");
     sendHttpJSON(randomValue, arduinoDate, sensorId);
-    previousMillisSend = now;                            
+    previousMillisSend = now;
   }
 }
 
-String setupArduinoDate() {
+String setupArduinoDate()
+{
   struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
+  if (!getLocalTime(&timeinfo))
+  {
     return ("Failed to obtain time");
   }
   strftime(buffer, sizeof buffer, "%FT%TZ", &timeinfo);
   return buffer;
 }
 
-void setUpWifi() {
+void setUpWifi()
+{
   WiFi.begin(ssid, password);
   Serial.println("Establishing connection to WiFi with SSID: " + String(ssid));
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.print(".");
   }
@@ -83,58 +92,66 @@ void setUpWifi() {
   Serial.println(WiFi.localIP());
 }
 
-void webSocketEvent(byte num, WStype_t type, uint8_t * payload, size_t length ) {
-  switch (type) {
-    case WStype_DISCONNECTED:
-      Serial.println("Client " + String(num) + " disconnected");
-      break;
-    case WStype_CONNECTED:
-      Serial.println("Client " + String(num) + " connected");
-      break;
-    case WStype_TEXT:
-      StaticJsonDocument<200> doc;
-      DeserializationError error = deserializeJson(doc, payload);
-      if (error) {
-        Serial.print(F("deserializeJson() failed: "));
-        Serial.println(error.f_str());
-        return;
-      }
-      else {
-         value_lower = doc["value_lower"];
-         value_upper = doc["value_upper"];
-        Serial.println("lower: " + String(value_upper));
-        Serial.println("upper: " + String(value_lower));
-      }
-      break;
+void webSocketEvent(byte num, WStype_t type, uint8_t *payload, size_t length)
+{
+  switch (type)
+  {
+  case WStype_DISCONNECTED:
+    Serial.println("Client " + String(num) + " disconnected");
+    break;
+  case WStype_CONNECTED:
+    Serial.println("Client " + String(num) + " connected");
+    break;
+  case WStype_TEXT:
+    StaticJsonDocument<200> doc;
+    DeserializationError error = deserializeJson(doc, payload);
+    if (error)
+    {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return;
+    }
+    else
+    {
+      value_lower = doc["value_lower"];
+      value_upper = doc["value_upper"];
+      Serial.println("lower: " + String(value_upper));
+      Serial.println("upper: " + String(value_lower));
+    }
+    break;
   }
 }
 
-void getSensorParams(String sensorId) {
+void getSensorParams(String sensorId)
+{
   WiFiClient client;
   HTTPClient http;
   http.begin("http://127.0.0.1:8000/api/get-params/?sensor-id=5");
   http.addHeader("Content-Type", "application/json");
-  String  jsonString = "";
+  String jsonString = "";
   DynamicJsonDocument doc(1024);
   doc["sensor"] = String(sensorId);
   serializeJson(doc, jsonString);
   int httpResponseCode = http.GET();
-  if (httpResponseCode > 0) {
+  if (httpResponseCode > 0)
+  {
     Serial.print("HTTP Response code: ");
     Serial.println(httpResponseCode);
   }
-  else {
+  else
+  {
     Serial.print("Error code: ");
     Serial.println(httpResponseCode);
   }
 }
 
-void sendHttpJSON(int value, String date, String sensorId) {
+void sendHttpJSON(int value, String date, String sensorId)
+{
   WiFiClient client;
   HTTPClient http;
   http.begin("https://messuring-vault.herokuapp.com/api/");
   http.addHeader("Content-Type", "application/json");
-  String  jsonString = "";
+  String jsonString = "";
   DynamicJsonDocument doc(1024);
   doc["sensor"] = String(sensorId);
   doc["value"] = String(value);
@@ -146,7 +163,8 @@ void sendHttpJSON(int value, String date, String sensorId) {
   http.end();
 }
 
-void sendWithWebscoket() {
+void sendWithWebscoket()
+{
   String jsonString = "";
   StaticJsonDocument<200> doc;
   JsonObject object = doc.to<JsonObject>();
